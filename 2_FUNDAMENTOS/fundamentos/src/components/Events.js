032259ec1e304ed990cec594a3942559d3351929
AllanFenelon <allanpereira.fenelon@gmail.com>
2024-05-15 14:18:05 -0300

    função de renderização

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.js
@@ -4,6 +4,14 @@ const Events = () => {
         console.log(e)
     } 
 
+    const renderSomething = (x) => {
+        if(x){
+            return <h1>Rederizando isso</h1>
+        }else{
+            return <h1>Também rederizando isso</h1>
+        }
+    }
+
     return (
         <div>
             <div>
@@ -17,6 +25,8 @@ const Events = () => {
                     }
                 }}>Clica aqui, por favor.</button>
             </div>
+            {renderSomething(true)}
+            {renderSomething(false)}
         </div>
     )
 
