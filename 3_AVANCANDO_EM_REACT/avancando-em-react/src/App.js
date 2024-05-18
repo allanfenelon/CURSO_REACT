2fdd78fd88120673932bbea2b75a0b7f9aebbfb8
AllanFenelon <allanpereira.fenelon@gmail.com>
2024-05-18 12:18:50 -0300

    props e destructuring props

--- 3_AVANCANDO_EM_REACT/avancando-em-react/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando-em-react/src/App.js
@@ -2,15 +2,21 @@
 import ManageData from './components/ManageData';
 import ListRender from './components/ListRender';
 import ConditionalRender from './components/ConditionalRender';
+import ShowUserName from './components/ShowUserName';
+import CarDetails from './components/CarDetails';
 // Assests / CSS
 import './App.css';
 import City from './assets/city.jpg' 
+import { useState } from 'react';
+
+
 
 
 
 
 
 function App() {
+  const [userName] = useState('Maria') 
   return (
     <div className="App">
       <div>
@@ -25,6 +31,11 @@ function App() {
         <ManageData />
         <ListRender />
         <ConditionalRender />
+        {/* PROPS */}
+        <ShowUserName name={userName} />
+        {/* destructuring */}
+        <CarDetails brand='BMW' km={1500} color='Preto' />
+
     </div>
   );
 }
