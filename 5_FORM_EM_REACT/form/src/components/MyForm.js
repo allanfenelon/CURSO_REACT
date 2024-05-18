f3f2b185040b18ab8468069d7e6d6113db5f2f11
AllanFenelon <allanpereira.fenelon@gmail.com>
2024-05-18 20:21:46 -0300

    gerenciamento de dados dos input

--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -1,18 +1,29 @@
 import styles from './MyForm.module.css';
+import {useState} from "react"
 
 const MyForm = () => {
+    // 3 - GERENCIAMENTO DE DADOS
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState('')
+
+    const handleName = (e) => {
+        setName(e.target.value)
+    }
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+    }
   return (
     <div className={styles.div_formulario}>
         {/* 1- CRIAÇÃO DO FORM */}
         <form className={styles.formulario}>
             <div>
                 <label htmlFor="name">Nome:</label>
-                <input type="text" name='name' placeholder='Digite o seu nome'/>
+                <input type="text" name='name' placeholder='Digite o seu nome' onChange={handleName}/>
             </div>
             {/* 2 - LABEL ENVOLVENDO O INPUT */}
             <label>
                 <span>E-mail</span>
-                <input type="email" name='email' placeholder='Digite o e-mail'/>
+                <input type="email" name='email' placeholder='Digite o e-mail' onChange={handleEmail} />
             </label>
             <input type="submit" value='Enviar' />
         </form>
