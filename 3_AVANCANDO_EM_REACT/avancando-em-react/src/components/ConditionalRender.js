406ed4ca3dc005c8f22acb1d9e6b3256c3b33238
AllanFenelon <allanpereira.fenelon@gmail.com>
2024-05-18 11:55:30 -0300

    if, else no JSX

--- 3_AVANCANDO_EM_REACT/avancando-em-react/src/components/ConditionalRender.js
+++ 3_AVANCANDO_EM_REACT/avancando-em-react/src/components/ConditionalRender.js
@@ -4,10 +4,14 @@ import { useState } from "react"
 const ConditionalRender = () => {
 const [x] = useState(true)
 
+const [name, setName] = useState('Allan')
+
   return (
     <div>
         <h1>Isso será exibido?</h1>
         {x && <p>Se x for true, sim!</p>}
+        {name === 'Allan' ? ('O nome é Allan') : ('O nome não é Allan')}
+        <button onClick={() => setName('Pedro')}>Clica aqui</button>
     </div>
   )
 }
