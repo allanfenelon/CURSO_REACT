415e5c5f77bae8a7441f67a90f5e19c200d1286d
AllanFenelon <allanpereira.fenelon@gmail.com>
2024-05-19 11:20:02 -0300

    SELECT NO REACT

--- 5_FORM_EM_REACT/form/src/App.js
+++ 5_FORM_EM_REACT/form/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <h2>Forms</h2>
-      <MyForm user={{name:'Allan', email:'allan@gmail.com', bio:'Tenho 23 anos e gosto de programação!'}}/>
+      <MyForm user={{name:'Allan', email:'allan@gmail.com', bio:'Tenho 23 anos e gosto de programação!', role:'admin'}}/>
     </div>
   );
 }
--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -7,6 +7,7 @@ const MyForm = ({user}) => {
     const [name, setName] = useState(user ? user.name : '')
     const [email, setEmail] = useState(user ? user.email : '')
     const [bio, setBio] = useState(user ? user.bio : '')
+    const [role,setRole] = useState(user ? user.role : '')
 
     const handleName = (e) => {
         setName(e.target.value)
@@ -18,13 +19,14 @@ const MyForm = ({user}) => {
         
         event.preventDefault(); // não faz o formulario recarregar a página
         console.log('Enviando dados...')
-        console.log(name,email)
+        console.log(name,email,bio, role)
 
         // validação, envio e etc
         // 7 - Limpando formulários após envio
         setName('')
         setEmail('')
         setBio('')
+        setRole('')
     }
   return (
     <div className={styles.div_formulario} onSubmit={handleSubmit}>
@@ -41,10 +43,20 @@ const MyForm = ({user}) => {
                 {/* 4- SIMPLIFICAÇÃO DE MANIPUTALÃO DE STATE */}
                 <input type="email" name='email' placeholder='Digite o e-mail' onChange={(e) => setEmail(e.target.value)} value={email}/>
             </label>
-            {/* TEXTAREA */}
+            {/*8 - TEXTAREA */}
             <label>
                 <span>Bio:</span>
                 <textarea name="bio" placeholder='Descrição pessoal' onChange={(e) => setBio(e.target.value)} value={bio}></textarea>
+            </label>
+            {/*9 - SELECT */}
+            <label>
+                <span>Função no sistema:</span>
+                <select name="role" onChange={(e) => setRole(e.target.value)} value={role}>
+                    <option value="user">Usuário</option>
+                    <option value="editor">Editor</option>
+                    <option value="admin">Administrador</option>
+                </select>
+
             </label>
             <input type="submit" value='Enviar' />
         </form>
